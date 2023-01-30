068f24ac6ace78bea16e0b42ec9ec3894bf7c684
ABay23 <alejandrobay23@gmail.com>
2023-01-30 15:36:58 -0500

    Added .env and port

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('...loading data!')
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello!!!')
+})
+
+app.listen(PORT, () => console.log(`The server is running on port: ${PORT}`))
